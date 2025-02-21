a32427baa9f3e55761f71d01b406201538e29f76
Alex-L-K-free <freeworkit025@gmail.com>
2025-02-21 16:37:29 +0300

    работа над оформлением - панели управления

--- educonnect/frontend/src/components/layout/Sidebar.js
+++ educonnect/frontend/src/components/layout/Sidebar.js
@@ -9,7 +9,15 @@ const Sidebar = ({ activePage }) => {
         <li className={activePage === 'home' ? 'active' : ''}>
           <a href="/">Главная</a>
         </li>
-        {/* Удалите другие элементы, если они не нужны на главной странице */}
+        <li className={activePage === 'admin' ? 'active' : ''}>
+          <a href="/admin">Панель управления</a>
+        </li>
+        <li className={activePage === 'teacher' ? 'active' : ''}>
+          <a href="/teacher">Панель учителя</a>
+        </li>
+        <li className={activePage === 'student' ? 'active' : ''}>
+          <a href="/student">Панель ученика</a>
+        </li>
       </ul>
     </div>
   );
