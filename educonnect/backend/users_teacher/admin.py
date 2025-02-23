5a59fe8e07e1828222a38a96d8c894654aa3c2a7
Alex-L-K-free <freeworkit025@gmail.com>
2025-02-23 19:44:39 +0300

    регистрация учителя (профиль v2.5)

--- educonnect/backend/users_teacher/admin.py
+++ educonnect/backend/users_teacher/admin.py
@@ -5,14 +5,32 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class TeacherUserAdmin(admin.ModelAdmin):
-    list_display = ('get_username', 'get_first_name', 'get_last_name', 'get_email')
-    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'user__email')
-    fields = (
-        'user',
-        ('telegram', 'viber'),
-        'about',
-        'specialization'
+    list_display = ('get_username', 'get_first_name', 'get_last_name', 'get_email', 'telegram', 'viber', 'about', 'specialization')
+    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'user__email', 'telegram', 'viber', 'about', 'specialization')
+    
+    fieldsets = (
+        ('Основная информация', {
+            'fields': (
+                'user',
+                ('get_first_name_display', 'get_last_name_display'),
+                'get_email_display',
+            )
+        }),
+        ('Контакты', {
+            'fields': (
+                'telegram',
+                'viber',
+            )
+        }),
+        ('Дополнительная информация', {
+            'fields': (
+                'about',
+                'specialization',
+            )
+        }),
     )
+
+    readonly_fields = ('get_first_name_display', 'get_last_name_display', 'get_email_display')
 
     def get_username(self, obj):
         return obj.user.username
@@ -21,12 +39,12 @@ class TeacherUserAdmin(admin.ModelAdmin):
 
     def get_first_name(self, obj):
         return obj.user.first_name
-    get_first_name.short_description = 'First Name'
+    get_first_name.short_description = 'Имя'
     get_first_name.admin_order_field = 'user__first_name'
 
     def get_last_name(self, obj):
         return obj.user.last_name
-    get_last_name.short_description = 'Last Name'
+    get_last_name.short_description = 'Фамилия'
     get_last_name.admin_order_field = 'user__last_name'
 
     def get_email(self, obj):
@@ -34,8 +52,19 @@ class TeacherUserAdmin(admin.ModelAdmin):
     get_email.short_description = 'Email'
     get_email.admin_order_field = 'user__email'
 
+    def get_first_name_display(self, obj):
+        return obj.user.first_name
+    get_first_name_display.short_description = 'Имя'
+
+    def get_last_name_display(self, obj):
+        return obj.user.last_name
+    get_last_name_display.short_description = 'Фамилия'
+
+    def get_email_display(self, obj):
+        return obj.user.email
+    get_email_display.short_description = 'Email'
+
     def save_model(self, request, obj, form, change):
-        # Сохраняем связанного пользователя
         if obj.user:
             obj.user.save()
         super().save_model(request, obj, form, change)
