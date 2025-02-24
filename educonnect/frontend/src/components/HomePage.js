48a1b5b6a376b1423a17e7d16315d54aebf35cb6
Alex-L-K-free <freeworkit025@gmail.com>
2025-02-24 21:59:34 +0300

    профиль учителя - оформление главной

--- educonnect/frontend/src/components/HomePage.js
+++ educonnect/frontend/src/components/HomePage.js
@@ -3,30 +3,25 @@ import backgroundImage from '../assets/images/background.jpg';
 import './HomePage.css';
 // import './Sidebar.css'; // Импортируйте стили для Sidebar
 // import Sidebar from './layout/Sidebar';
+// import eduConnectLogo from '../assets/images/educonnect-logo.png';
 
 const HomePage = () => {
   return (
-    <div className="home-page" style={{
-      backgroundImage: `url(${backgroundImage})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      minHeight: 'calc(100vh - var(--header-height) - var(--footer-height))',
-      color: '#fff',
-      padding: '20px',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      textAlign: 'center'
-    }}>
-      <div className="content-wrapper">
-        <h1>Добро пожаловать в образовательную платформу</h1>
-        <h2>Платформа предоставляет:</h2>
-        <ul>
-          <li>Удобный доступ к учебным материалам</li>
-          <li>Взаимодействие между учителем и учениками</li>
-          <li>Управление учебным процессом</li>
-        </ul>
+    <div className="home-page">
+      <div className="background-image" 
+        style={{ backgroundImage: `url(${backgroundImage})` }} 
+      />
+      <div className="content">
+        <div className="content-box">
+          <h1>Добро пожаловать в образовательную платформу</h1>
+          <h1>EduConnect</h1>
+          <h2>Платформа предоставляет:</h2>
+          <ul>
+            <li>Удобный доступ к учебным материалам</li>
+            <li>Взаимодействие между учителем и учениками</li>
+            {/* <li>Управление учебным процессом</li> */}
+          </ul>
+        </div>
       </div>
     </div>
   );
