582af1bae503f5b62cd4c958b11086d674c0ff18
Alex-L-K-free <freeworkit025@gmail.com>
2025-02-24 21:37:57 +0300

    профиль учителя - оформление главной

--- educonnect/frontend/src/components/HomePage.js
+++ educonnect/frontend/src/components/HomePage.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import backgroundImage from '../assets/images/background.jpg';
+import './HomePage.css';
 // import './Sidebar.css'; // Импортируйте стили для Sidebar
 // import Sidebar from './layout/Sidebar';
 
 const HomePage = () => {
   return (
-    <div className="home-page">
-      {/* <Sidebar activePage="home" /> */}
-      {/* <div className="main-content"> */}
-        <h1>Добро пожаловать на главную страницу</h1>
-        {/* Добавьте функционал для главной страницы */}
-      {/* </div> */}
+    <div className="home-page" style={{
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      minHeight: 'calc(100vh - var(--header-height) - var(--footer-height))',
+      color: '#fff',
+      padding: '20px',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      textAlign: 'center'
+    }}>
+      <div className="content-wrapper">
+        <h1>Добро пожаловать в образовательную платформу</h1>
+        <h2>Платформа предоставляет:</h2>
+        <ul>
+          <li>Удобный доступ к учебным материалам</li>
+          <li>Взаимодействие между учителем и учениками</li>
+          <li>Управление учебным процессом</li>
+        </ul>
+      </div>
     </div>
   );
 };
