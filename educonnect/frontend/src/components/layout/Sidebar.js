89822cb338823bc0c29b8a61e5cc7a8f23ccbcf3
Alex-L-K-free <freeworkit025@gmail.com>
2025-03-16 10:23:55 +0300

    оптимизация sidebar v1

--- educonnect/frontend/src/components/layout/Sidebar.js
+++ educonnect/frontend/src/components/layout/Sidebar.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Sidebar.css'; // Импортируйте стили для Sidebar
 import educonnectLogo from '../../assets/images/educonnect-logo.png';
+import LoginModal from '../LoginModal';
 
 const Sidebar = ({ activePage }) => {
+  const [showLogin, setShowLogin] = useState(false);
+
+  const handleShow = () => setShowLogin(true);
+  const handleClose = () => setShowLogin(false);
+
   return (
     <div className="sidebar">
-      <div className="sidebar-header">
+      <div 
+        className="sidebar-header clickable"
+        onClick={handleShow}
+        role="button"
+        tabIndex={0}
+      >
         <img 
           src={educonnectLogo} 
           alt="EduConnect" 
           className="sidebar-logo"
         />
       </div>
-      <div className="registration-warning">
-       <h6> Необходимо войти или зарегистрироваться для продолжения!</h6>
+      <div 
+        className="registration-warning clickable"
+        onClick={handleShow}
+        role="button"
+        tabIndex={0}
+      >
+        <h6>Необходимо войти или зарегистрироваться для продолжения!</h6>
       </div>
       {/* <h2>главная</h2>
       <ul>
@@ -30,6 +46,11 @@ const Sidebar = ({ activePage }) => {
           <a href="/student">Панель ученика</a>
         </li>
       </ul> */}
+
+      <LoginModal 
+        show={showLogin} 
+        onHide={handleClose}
+      />
     </div>
   );
 };
