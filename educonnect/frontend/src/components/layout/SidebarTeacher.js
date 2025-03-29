66dc4aec403e23ec77400e9ba82d284cbc5d54d8
Alex-L-K-free <freeworkit025@gmail.com>
2025-03-29 20:00:38 +0300

    учет запись ученика v2

--- educonnect/frontend/src/components/layout/SidebarTeacher.js
+++ educonnect/frontend/src/components/layout/SidebarTeacher.js
@@ -231,7 +231,7 @@ const SidebarTeacher = ({ activePage, onNavigate, availableSubjects, availableCl
           onClick={handleMainClick}
         >
           <span className="menu-icon">🏠</span>
-          <span className="menu-text">Начальная</span>
+          <span className="menu-text">Главная</span>
         </li>
         
         {menuItems.map(menu => (
